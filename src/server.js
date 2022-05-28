fb297c688d413c8913aefbb0d7e46a7709497878
dwisatriow <dwisatrioow@gmail.com>
2022-05-29 06:50:49 +0700

    Setup routes

--- src/server.js
+++ src/server.js
@@ -12,4 +12,31 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+/**
+ * Get started
+ */
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+/**
+ * Setup routes
+ */
+app.get('/data', (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('listening on port 3000')
+  })
+}
